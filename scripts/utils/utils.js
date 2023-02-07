a792fedecf7aa0485f6dcc4e9f338b3203f6786b
Margarita Shumilina <margar.maksim@ya.ru>
2023-02-07 19:59:40 +0300

    Updates

--- scripts/utils/utils.js
+++
@@ -1,3 +0,0 @@
-export const popupFullImage = document.querySelector('.popup-full-img');
-export const popupFullPhoto = document.querySelector('.popup-full-img__photo');
-export const popupFullPhotoTitle = document.querySelector('.popup-full-img__title');
